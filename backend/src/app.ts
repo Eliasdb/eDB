22980c24ebddb3844680eb29c91b7d8d6f38bbfa
Elias <elias.db3@gmail.com>
2024-10-31 15:11:06 +0100

    kubernetes init part 1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,8 +9,12 @@ const app = express();
 app.use(express.json());
 app.use('/api/users', userRoutes);
 
+app.get('/health', (req, res) => {
+    res.status(200).send('Healthy');
+});
+
 const PORT = 9100;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
