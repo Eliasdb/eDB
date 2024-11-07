870fbe9555c15a52c1f86ed0aa71652f794d04b4
Elias <elias.db3@gmail.com>
2024-11-07 14:35:26 +0100

    safety commit

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -21,7 +21,7 @@ const count = ref(0);
         Check out
         <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
             >create-vue</a
-        >, the official Vueeeee + reacrrt starter
+        >, the official Vueeee + reacrrt starter
     </p>
     <p>
         Learn more about IDE Support for Vue in the
