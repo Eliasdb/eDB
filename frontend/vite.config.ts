b7b91ef279f7d3989de04801718740c868f0929d
Elias <elias.db3@gmail.com>
2024-11-08 14:58:04 +0100

    refactor

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,12 +8,9 @@ export default defineConfig({
         host: '0.0.0.0', // Bind to all network interfaces
         port: 8080, // Ensure this matches the port you'll expose
         strictPort: true, // Exit if port is already in use
-<<<<<<< HEAD
         watch: {
             usePolling: true, // Use polling for better compatibility in containerized environments
             interval: 100, // Adjust as needed
         },
-=======
->>>>>>> ea02745 (Save untracked files before pull)
     },
 });
