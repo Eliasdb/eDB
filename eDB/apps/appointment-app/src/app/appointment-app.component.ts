49f3dd33510f549ea3a72798a5fbde5b5431d7bb
Elias <elias.db3@gmail.com>
2024-11-13 00:21:40 +0100

    init ui lib

--- eDB/apps/appointment-app/src/app/appointment-app.component.ts
+++ eDB/apps/appointment-app/src/app/appointment-app.component.ts
@@ -1,9 +1,12 @@
 // appointment-app/src/app/appointment-app.component.ts
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@e-db/ui'; // Import the standalone component directly
 
 @Component({
   selector: 'app-appointment-home',
+  imports: [ButtonComponent],
   standalone: true,
-  template: `<h1>Appointment Home</h1> `,
+  template: `<h1>Appointment Home</h1>
+    <lib-button>Click Me!rfenjfnejkf</lib-button>`,
 })
 export class AppointmentHomeComponent {}
--- eDB/apps/eDB/src/app/home.component.ts
+++ eDB/apps/eDB/src/app/home.component.ts
@@ -1,9 +1,12 @@
 // shell-app/src/app/home.component.ts
 import { Component } from '@angular/core';
+import { ButtonComponent } from '@e-db/ui'; // Import the standalone component directly
 
 @Component({
   standalone: true,
+  imports: [ButtonComponent],
   selector: 'app-home',
-  template: `<h1>Welcome to eDB Platform</h1>`,
+  template: `<h1>Welcome to eDB Platform</h1>
+    <lib-button>Click Me!rfenjfnejkf</lib-button> `,
 })
 export class HomeComponent {}
