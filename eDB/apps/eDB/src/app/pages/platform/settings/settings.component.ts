3f70e5552c35dc1e39ff9ef543576372aa29a043
Elias <elias.db3@gmail.com>
2024-11-18 17:52:02 +0100

    update settings page + structured list component

--- eDB/apps/eDB/src/app/pages/platform/settings/settings.component.ts
+++ eDB/apps/eDB/src/app/pages/platform/settings/settings.component.ts
@@ -1,10 +1,25 @@
-// shell-app/src/app/home.component.ts
 import { Component } from '@angular/core';
+import { UiStructuredListComponent } from '@e-db/ui';
 
 @Component({
   selector: 'platform-settings',
   standalone: true,
-  imports: [],
-  template: `<h1>Welcome to settings</h1> `,
+  imports: [UiStructuredListComponent],
+  template: `
+    <section class="settings-page">
+      <h1>Welcome to settings</h1>
+      <section class="settings">
+        <ui-structured-list
+          [header]="'ID and Password'"
+          [headerIcon]="'faKey'"
+          [rows]="[
+            ['E-mail', 'Active'],
+            ['Password', 'Inactive']
+          ]"
+        ></ui-structured-list>
+      </section>
+    </section>
+  `,
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent {}
---
+++ eDB/libs/ui/src/lib/components/structured-list/structured-list.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { StructuredListModule } from 'carbon-components-angular';
+import { UiIconComponent } from '../icon/icon.component';
+
+@Component({
+  selector: 'ui-structured-list',
+  standalone: true,
+  imports: [StructuredListModule, UiIconComponent, CommonModule],
+  template: `
+    <cds-structured-list>
+      <cds-list-header>
+        <!-- First column header passed as input -->
+        <cds-list-column>
+          {{ header }}
+          <!-- Dynamic icon in the header -->
+          <ui-icon
+            *ngIf="headerIcon"
+            [name]="headerIcon"
+            size="16"
+            class="icon-gap"
+          ></ui-icon>
+        </cds-list-column>
+      </cds-list-header>
+
+      <!-- Dynamic rows passed as input -->
+      <cds-list-row *ngFor="let row of rows">
+        <cds-list-column>{{ row[0] }}</cds-list-column>
+
+        <cds-list-column>{{ row[1] }}</cds-list-column>
+
+        <cds-list-column>
+          Edit
+          <ui-icon name="faEdit" size="16" class="icon-gap"></ui-icon>
+        </cds-list-column>
+      </cds-list-row>
+    </cds-structured-list>
+  `,
+  styleUrls: ['./structured-list.component.scss'],
+})
+export class UiStructuredListComponent {
+  @Input() header: string = ''; // First column header
+  @Input() headerIcon: string = ''; // Dynamic icon for the header
+  @Input() rows: string[][] = []; // Array of rows for the first and second columns
+
+  constructor() {}
+}
