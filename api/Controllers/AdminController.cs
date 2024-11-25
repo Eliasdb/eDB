4dbd8a0780a69545bac48df80448518d73783424
Elias <elias.db3@gmail.com>
2024-11-25 15:10:46 +0100

    first try infinitequery

--- api/Controllers/AdminController.cs
+++ api/Controllers/AdminController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using api.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace api.Controllers
 {
@@ -21,5 +24,36 @@ namespace api.Controllers
             Console.WriteLine("AdminArea endpoint hit");
             return Ok("Welcome, Admin!");
         }
+
+        // Updated endpoint to get the list of users with pagination
+        [HttpGet("users")]
+        [RoleAuthorize("Admin")]
+        public async Task<IActionResult> GetUsers(int pageNumber = 1, int pageSize = 15)
+        {
+            try
+            {
+                var totalUsers = await _context.Users.CountAsync();
+
+                var users = await _context.Users
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                bool hasMore = (pageNumber * pageSize) < totalUsers;
+
+                var result = new
+                {
+                    Items = users,
+                    HasMore = hasMore
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if needed
+                return StatusCode(500, $"An error occurred while retrieving users: {ex.Message}");
+            }
         }
+    }
 }
--- eDB/libs/ui/src/lib/components/tables/user-table/user-table.component.ts
+++ eDB/libs/ui/src/lib/components/tables/user-table/user-table.component.ts
@@ -1,31 +1,12 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-  TemplateRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PaginationModule } from 'carbon-components-angular';
-import {
-  TableHeaderItem,
-  TableItem,
-  TableModel,
-  TableModule,
-} from 'carbon-components-angular/table';
-import { UiPlatformOverflowMenuComponent } from '../../navigation/overflow-menu/overflow-menu.component';
+import { TableModel, TableModule } from 'carbon-components-angular/table';
 
 @Component({
   selector: 'ui-table',
   standalone: true,
-  imports: [
-    CommonModule,
-    TableModule,
-    PaginationModule,
-    UiPlatformOverflowMenuComponent,
-  ],
+  imports: [CommonModule, TableModule, PaginationModule],
   template: `
     <cds-table-container>
       <cds-table-header>
@@ -38,103 +19,36 @@ import { UiPlatformOverflowMenuComponent } from '../../navigation/overflow-menu/
         [sortable]="sortable"
         [showSelectionColumn]="showSelectionColumn"
         [stickyHeader]="stickyHeader"
-        (rowClick)="onRowClick($event)"
         [isDataGrid]="isDataGrid"
+        (rowClick)="onRowClick($event)"
       ></cds-table>
 
       <cds-pagination
+        *ngIf="showPagination"
         [model]="model"
         (selectPage)="onPageChange($event)"
       ></cds-pagination>
     </cds-table-container>
-
-    <ng-template #overflowTemplate let-user="data">
-      <ui-platform-overflow-menu
-        [menuOptions]="menuOptions"
-        icon="faEllipsisVertical"
-        (menuOptionSelected)="onOverflowMenuSelect($event, user)"
-      ></ui-platform-overflow-menu>
-    </ng-template>
   `,
 })
-export class UiTableComponent implements OnInit {
+export class UiTableComponent {
   @Input() title = 'Table Title';
   @Input() description = 'Table description goes here.';
+  @Input() model!: TableModel;
   @Input() sortable = true;
   @Input() showSelectionColumn = true;
   @Input() stickyHeader = false;
-  @Input() pageLength = 10;
-  @Input() users: any[] = [];
-  @Input() totalDataLength = 0;
   @Input() isDataGrid = false;
+  @Input() showPagination = true;
 
-  @Output() rowDeleted = new EventEmitter<number>();
-  @Output() roleChanged = new EventEmitter<{ id: number; newRole: string }>();
+  @Output() rowClicked = new EventEmitter<number>();
+  @Output() pageChanged = new EventEmitter<number>();
 
-  @ViewChild('overflowTemplate', { static: true })
-  overflowTemplate!: TemplateRef<any>;
-
-  model = new TableModel();
-
-  menuOptions = [
-    { id: 'delete', label: 'Delete' },
-    { id: 'changeRole', label: 'Change Role' },
-  ];
-
-  ngOnInit(): void {
-    this.initializeTableModel();
-    this.loadPageData(1);
-  }
-
-  initializeTableModel(): void {
-    this.model.pageLength = this.pageLength;
-    this.model.totalDataLength = this.totalDataLength;
-    this.model.header = [
-      new TableHeaderItem({ data: 'Name', sortable: this.sortable }),
-      new TableHeaderItem({ data: 'Email', sortable: this.sortable }),
-      new TableHeaderItem({ data: 'Country', sortable: this.sortable }),
-      new TableHeaderItem({ data: 'Role', sortable: this.sortable }),
-      new TableHeaderItem({ data: '', sortable: false }),
-    ];
-  }
-
-  loadPageData(page: number): void {
-    const start = (page - 1) * this.pageLength;
-    const end = start + this.pageLength;
-    const pageData = this.users.slice(start, end);
-    this.model.data = this.prepareData(pageData);
-    this.model.currentPage = page;
+  onRowClick(index: number): void {
+    this.rowClicked.emit(index);
   }
 
   onPageChange(page: number): void {
-    this.loadPageData(page);
-  }
-
-  onRowClick(index: number): void {
-    console.log('Row clicked:', this.model.data[index]);
-  }
-
-  onOverflowMenuSelect(actionId: string, user: any): void {
-    if (actionId === 'delete') {
-      this.rowDeleted.emit(user.id);
-    } else if (actionId === 'changeRole') {
-      const newRole = prompt(
-        'Enter new role (User, Admin, PremiumUser):',
-        user.role
-      );
-      if (newRole) {
-        this.roleChanged.emit({ id: user.id, newRole });
-      }
-    }
-  }
-
-  prepareData(users: any[]): TableItem[][] {
-    return users.map((user) => [
-      new TableItem({ data: `${user.firstName} ${user.lastName}` }),
-      new TableItem({ data: user.email }),
-      new TableItem({ data: user.country }),
-      new TableItem({ data: user.role }),
-      new TableItem({ data: user, template: this.overflowTemplate }),
-    ]);
+    this.pageChanged.emit(page);
   }
 }
