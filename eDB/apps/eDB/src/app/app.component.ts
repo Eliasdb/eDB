4d934418f051842169028f3e7a92672c5b1bc205
Elias <elias.db3@gmail.com>
2024-12-02 16:48:33 +0100

    bug fix overflow menu

--- eDB/apps/eDB/src/app/app.component.ts
+++ eDB/apps/eDB/src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  ExperimentalService,
+  I18nModule,
+  ModalService,
+  PlaceholderModule,
+  PlaceholderService,
+} from 'carbon-components-angular';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterModule],
-  template: `<router-outlet></router-outlet>`,
+  imports: [RouterModule, PlaceholderModule, I18nModule],
+  providers: [ModalService, ExperimentalService, PlaceholderService],
+  template: `
+    <router-outlet></router-outlet>
+    <cds-placeholder></cds-placeholder>
+  `,
 })
 export class AppComponent {
   title = 'eDB';
--- eDB/libs/ui/src/lib/components/tables/table/table.component.stories.ts
+++ eDB/libs/ui/src/lib/components/tables/table/table.component.stories.ts
@@ -1,104 +1,38 @@
 // ui-table.component.stories.ts
 
 import { Meta, StoryObj } from '@storybook/angular';
-import {
-  TableHeaderItem,
-  TableItem,
-  TableModel,
-} from 'carbon-components-angular/table';
-import { UiTableComponent } from './table.component';
+import { OverflowTableStoryComponent } from './overflow-table-story.component';
 
-const meta: Meta<UiTableComponent> = {
-  title: 'Components/Tables/Expandable Table',
-  component: UiTableComponent,
+const meta: Meta<OverflowTableStoryComponent> = {
+  title: 'Components/Tables/Table with Overflow Menu',
+  component: OverflowTableStoryComponent,
+  parameters: {
+    controls: {
+      include: [
+        'size',
+        'showSelectionColumn',
+        'striped',
+        'isDataGrid',
+        'sortable',
+        'stickyHeader',
+        'skeleton',
+      ],
+    },
+  },
 };
 
 export default meta;
 
-type Story = StoryObj<UiTableComponent>;
+type Story = StoryObj<OverflowTableStoryComponent>;
 
-const createExpandableTableModel = (): TableModel => {
-  const model = new TableModel();
-
-  model.header = [
-    new TableHeaderItem({
-      data: 'Main Column',
-      metadata: { sortField: 'mainColumn' },
-      sorted: false,
-      ascending: true,
-      descending: false,
-      sortable: true,
-      visible: true,
-      title: 'Main Column',
-    }),
-    new TableHeaderItem({
-      data: 'Additional Info',
-      metadata: { sortField: 'additionalInfo' },
-      sorted: false,
-      ascending: true,
-      descending: false,
-      sortable: false,
-      visible: true,
-      title: 'Additional Info',
-    }),
-  ];
-
-  model.data = [
-    [
-      new TableItem({ data: 'Name 1' }),
-      new TableItem({
-        data: { name: 'Lessy', link: '#' },
-        template: null, // Add custom template here if needed
-      }),
-    ],
-
-    [
-      new TableItem({
-        data: 'Name 3',
-      }),
-      new TableItem({ data: 'swer' }),
-    ],
-    [
-      new TableItem({
-        data: 'Name 3.1',
-        expandedData: [
-          [
-            new TableItem({ data: 'More names', expandedData: 'No template' }),
-            new TableItem({
-              data: { name: 'Morey', link: '#' },
-              // template: null, // Add custom template here if needed
-            }),
-          ],
-          [
-            new TableItem({ data: 'Core names', expandedData: 'No template' }),
-            new TableItem({
-              data: { name: 'Corey', link: '#' },
-              // template: null, // Add custom template here if needed
-            }),
-          ],
-        ],
-        expandAsTable: true,
-      }),
-      new TableItem({ data: 'swer' }),
-    ],
-  ];
-
-  model.pageLength = 5;
-  model.totalDataLength = 15;
-
-  return model;
-};
-
-export const Expandable: Story = {
+export const Default: Story = {
   args: {
-    title: 'Expandable Table',
-    description: 'A table with expandable rows for additional information.',
-    model: createExpandableTableModel(),
-    sortable: true,
+    size: 'md',
     showSelectionColumn: false,
-    stickyHeader: false,
+    striped: true,
     isDataGrid: false,
-    showPagination: false,
+    sortable: true,
+    stickyHeader: false,
     skeleton: false,
   },
 };
