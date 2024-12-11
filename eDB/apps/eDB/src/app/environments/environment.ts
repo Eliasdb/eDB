c67365c9b4f8145e7cc5b3554051bb6343ddec71
Elias <elias.db3@gmail.com>
2024-12-11 14:16:32 +0100

    added environments - backend works too now - app is fully live

---
+++ eDB/apps/eDB/src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:9101/api',
+  apiAdminUrl: 'http://localhost:9101/api/admin',
+  apiAuthUrl: 'http://localhost:9101/api/auth',
+};
