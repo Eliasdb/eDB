e2aaeaaa5b8d8f7a825ecc61e7ae15a18d44bf1d
Elias <elias.db3@gmail.com>
2025-01-02 17:23:20 +0100

    pipeline

--- api/Services/AuthService.cs
+++ api/Services/AuthService.cs
@@ -3,7 +3,6 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using api.Models;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace api.Services
