c7bf49253fdf99ab0e22766f28036f9c85caf7c8
Elias <elias.db3@gmail.com>
2025-02-03 17:58:24 +0100

    updated readme

--- eDB/apps/platform-api/Errors/ApiException.cs
+++ eDB/apps/platform-api/Errors/ApiException.cs
@@ -2,7 +2,7 @@ namespace PlatformAPI.Errors;
 
 public class ApiException(int statusCode, string? message = null, string? details = null)
 {
-    public int StatusCode { get; set; } = statusCode;
-    public string? Message { get; set; } = message;
-    public string? Details { get; set; } = details;
+  public int StatusCode { get; set; } = statusCode;
+  public string? Message { get; set; } = message;
+  public string? Details { get; set; } = details;
 }
