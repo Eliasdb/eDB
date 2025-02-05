d8c8d5daead31a7bc10281214189b7bd61c1b850
Elias <elias.db3@gmail.com>
2025-02-05 16:12:14 +0100

    pipeline

--- eDB/apps/platform-api/DTOs/Applications/ApplicationDto.cs
+++ eDB/apps/platform-api/DTOs/Applications/ApplicationDto.cs
@@ -7,5 +7,5 @@ public class ApplicationDto
   public string Description { get; set; } = string.Empty;
   public string IconUrl { get; set; } = string.Empty;
   public string RoutePath { get; set; } = string.Empty; // Path to access this app
-  public List<string> Tags { get; set; } = new(); // List of tags associated with the application
+  public List<string> Tags { get; set; } = new();
 }
