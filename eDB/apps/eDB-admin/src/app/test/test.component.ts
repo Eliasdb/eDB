500e894100ca0755c1c4d9c039418667a8ce162d
Elias <elias.db3@gmail.com>
2025-02-06 15:31:15 +0100

    pipeline

--- eDB/apps/eDB-admin/src/app/test/test.component.ts
+++ eDB/apps/eDB-admin/src/app/test/test.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-home',
   template: `
-    <h1 style="margin-top: 16rem;">Welcome to the Admin Home Page!!</h1>
+    <h1 style="margin-top: 16rem;">Welcome to the Admin Home Page!!!</h1>
   `,
 })
 export class AdminHomeComponent {}
