7e5d2473ffbf8e3d7bb73e121ea3c99a67776c6e
Elias <elias.db3@gmail.com>
2025-02-22 13:12:39 +0100

    Admin app bug fixes

--- eDB/apps/eDB-admin/src/app/app.routes.ts
+++ eDB/apps/eDB-admin/src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Route } from '@angular/router';
-import { AdminHomeComponent } from '../app/test/test.component';
 
 export const routes: Route[] = [
   {
     path: '',
-    component: AdminHomeComponent,
-  },
-  {
-    path: 'dashboard',
     loadChildren: () =>
       import('@eDB/feature-admin-dashboard').then(
         (m) => m.featureAdminDashboardRoutes,
