6c01f01d0b63b9edd613d60dd2cfd88363f472a9
Elias <elias.db3@gmail.com>
2025-02-24 00:19:59 +0100

    Pipeline fix

--- eDB/libs/shared/utils/environment/src/lib/environments/environment.staging.ts
+++ eDB/libs/shared/utils/environment/src/lib/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://api.staging.eliasdebock.com/api',
-  apiAdminUrl: 'https://api.staging.eliasdebock.com/api/admin',
-  apiAuthUrl: 'https://api.staging.eliasdebock.com/api/auth',
+  apiBaseUrl: 'https://api.staging.eliasdebock.com/platform/api',
+  apiAdminUrl: 'https://api.staging.eliasdebock.com/platform/api/admin',
+  apiAuthUrl: 'https://api.staging.eliasdebock.com/platform/api/auth',
 };
