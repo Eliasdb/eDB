df1b86c995d0df5a9c2bff436068c48ee68e9ede
Elias <elias.db3@gmail.com>
2025-02-27 11:24:31 +0100

    Mobile fix table (it was Tailwind)

--- eDB/libs/eDB-admin/features/feature-admin-dashboard/src/lib/admin.page.ts
+++ eDB/libs/eDB-admin/features/feature-admin-dashboard/src/lib/admin.page.ts
@@ -12,85 +12,12 @@ import { AdminBooksCollectionContainer } from './components/admin-books/admin-bo
 import { AdminSidebarComponent } from './components/admin-sidebar/admin-sidebar.component';
 import { AdminStatsContainer } from './components/admin-stats-container/admin-stats.container';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-
 @Component({
   selector: 'platform-admin',
   template: ` <section class="admin-page">
     <section>
       <h1 class="text-3xl">Admin</h1>
     </section>
-    <section class="collection-title">
-      <h3>Books</h3>
-      <div>
-        <button mat-raised-button color="accent">Add book</button>
-      </div>
-    </section>
-    <div class="example-container mat-elevation-z8">
-      <div class="example-table-container">
-        <table mat-table [dataSource]="test" class="mat-elevation-z8" matSort>
-          <ng-container matColumnDef="select">
-            <th mat-header-cell *matHeaderCellDef>
-              <mat-checkbox> </mat-checkbox>
-            </th>
-            <td mat-cell *matCellDef="let row">
-              <mat-checkbox> </mat-checkbox>
-            </td>
-          </ng-container>
-          <ng-container matColumnDef="id">
-            <th mat-header-cell *matHeaderCellDef mat-sort-header>#</th>
-            <td mat-cell *matCellDef="let row">{{ row.id }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="title">
-            <th mat-header-cell *matHeaderCellDef>Title</th>
-            <td mat-cell *matCellDef="let row">{{ row.title }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="genre">
-            <th mat-header-cell *matHeaderCellDef>Genre</th>
-            <td mat-cell *matCellDef="let row">{{ row.genre }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="author">
-            <th mat-header-cell *matHeaderCellDef>Author</th>
-            <td mat-cell *matCellDef="let row">{{ row.author }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="status">
-            <th mat-header-cell *matHeaderCellDef>Status</th>
-            <td mat-cell *matCellDef="let row">{{ row.status }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="year">
-            <th mat-header-cell *matHeaderCellDef>Year</th>
-            <td mat-cell *matCellDef="let row">{{ row.publishedDate }}</td>
-          </ng-container>
-
-          <ng-container matColumnDef="edit">
-            <th mat-header-cell *matHeaderCellDef></th>
-            <td mat-cell *matCellDef="let row">
-              <div class="flex justify-center">
-                <mat-icon>edit</mat-icon>
-              </div>
-            </td>
-          </ng-container>
-
-          <!-- Created Column -->
-          <tr
-            mat-header-row
-            *matHeaderRowDef="displayedColumns; sticky: true"
-          ></tr>
-          <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
-        </table>
-      </div>
-    </div>
     <admin-stats-container />
     <ui-content-switcher [options]="['Platform', 'Webshop']">
       <ng-container section1>
@@ -147,14 +74,6 @@ import { MatTableModule } from '@angular/material/table';
     CommonModule,
     AdminStatsContainer,
     AdminBooksCollectionContainer,
-
-    MatProgressSpinnerModule,
-    MatTableModule,
-    MatSortModule,
-    MatCheckboxModule,
-    MatPaginatorModule,
-    MatButtonModule,
-    MatIconModule,
   ],
   styleUrls: ['admin.page.scss'],
 })
@@ -167,54 +86,4 @@ export class AdminPage {
     this.currentView.set(newContent);
     this.drawer.toggle();
   }
-
-  test = [
-    {
-      id: 6,
-      photoUrl: 'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-      description:
-        'In quos et unde et id velit eum placeat molestias et qui corporis.',
-      userId: null,
-      title: "Verlie O'Conner DVM",
-      genre: 'Fantasy',
-      author: 'Luz Anderson',
-      status: 'available',
-      publishedDate: '1985',
-    },
-    {
-      id: 7,
-      photoUrl: 'https://example.com/image2.jpg',
-      description: 'A sample description for the second book.',
-      userId: null,
-      title: 'Sample Book Title',
-      genre: 'Mystery',
-      author: 'Jane Doe',
-      status: 'checked out',
-      publishedDate: '1992',
-    },
-    {
-      id: 8,
-      photoUrl: 'https://example.com/image3.jpg',
-      description: 'Another sample description for the third book.',
-      userId: 3,
-      title: 'Another Book Title',
-      genre: 'Science Fiction',
-      author: 'John Smith',
-      status: 'available',
-      publishedDate: '2001',
-    },
-  ];
-
-  // Example usage: log the array to the console
-
-  displayedColumns: string[] = [
-    'select',
-    'id',
-    'title',
-    'genre',
-    'author',
-    'status',
-    'year',
-    'edit',
-  ];
 }
