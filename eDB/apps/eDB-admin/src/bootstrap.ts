6f64a5a8d4c75d95b3c494a064ed4820f3ff3ffd
Elias <elias.db3@gmail.com>
2025-06-17 12:56:47 +0200

    test

--- eDB/apps/eDB-admin/src/bootstrap.ts
+++ eDB/apps/eDB-admin/src/bootstrap.ts
@@ -3,7 +3,7 @@ import { Chart, registerables } from 'chart.js';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-// check remote-entry for actual register. this one is so it works separately on localhost:4300. When you load your remote only for its Routes (via loadRemote('eDB-admin/Routes')), none of your remote’s main.ts bootstrap code ever runs. test
+// check remote-entry for actual register. this one is so it works separately on localhost:4300. When you load your remote only for its Routes (via loadRemote('eDB-admin/Routes')), none of your remote’s main.ts bootstrap code ever runs. test?
 Chart.register(...registerables);
 console.log('✅ chart.js registerables registered in remote:', Chart);
 
