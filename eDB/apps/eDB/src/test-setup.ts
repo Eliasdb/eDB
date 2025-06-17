296cb286048eb26e4c6a58f207f38c01d8d41dd7
Elias <elias.db3@gmail.com>
2025-06-17 16:14:49 +0200

    test

--- eDB/apps/eDB/src/test-setup.ts
+++ eDB/apps/eDB/src/test-setup.ts
@@ -1,6 +1,6 @@
 /// <reference types='vitest' />
 
-// Step 3: Now import Angular testing environment setup
+// Step 3: Now import Angular testing environment setup.
 import '@analogjs/vitest-angular/setup-zone';
 import { getTestBed } from '@angular/core/testing';
 import {
