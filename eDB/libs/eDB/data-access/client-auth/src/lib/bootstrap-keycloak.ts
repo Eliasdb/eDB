602a95380f3a10be38ad47aab4905ad0052bcbfe
Elias <elias.db3@gmail.com>
2025-06-21 23:01:13 +0200

    test

--- eDB/libs/eDB/data-access/client-auth/src/lib/bootstrap-keycloak.ts
+++ eDB/libs/eDB/data-access/client-auth/src/lib/bootstrap-keycloak.ts
@@ -1,7 +1,6 @@
 // libs/client-auth/src/lib/bootstrap-keycloak.ts
-import { ApplicationConfig, importProvidersFrom, Type } from '@angular/core';
+import { ApplicationConfig, Type } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { I18n, I18nModule, PlaceholderModule } from 'carbon-components-angular';
 import { KeycloakService } from './client';
 
 export async function bootstrapWithKeycloak(
@@ -22,14 +21,9 @@ export async function bootstrapWithKeycloak(
     // we don’t spread appConfig here because we want full control
     providers: [
       // bring in the NgModule‐level providers:
-      importProvidersFrom(
-        I18nModule,
-        PlaceholderModule,
-        // …any other Carbon NgModules you need
-      ),
+      // importProvidersFrom(),
 
-      // and *also* manually re-provide the I18n token itself:
-      I18n,
+      // …any other Carbon NgModules you need
 
       // now all your original appConfig providers:
       ...(appConfig.providers ?? []),
