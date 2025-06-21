7be25fd8467da49441fb1344d5475cc78a6cfc86
Elias <elias.db3@gmail.com>
2025-06-21 13:33:29 +0200

    fix: admin api

--- eDB/apps/admin-api/Controllers/BaseApiController.cs
+++ eDB/apps/admin-api/Controllers/BaseApiController.cs
@@ -3,6 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 namespace Edb.AdminAPI.Controllers
 {
   [ApiController]
-  [Route("api/[controller]")]
+  [Route("api")]
   public abstract class BaseApiController : ControllerBase { }
 }
