98608d38dcd3b043bedb23f3fa81da4776873523
Elias <elias.db3@gmail.com>
2025-06-22 18:59:48 +0200

    test

--- eDB/apps/eDB/src/bootstrap.ts
+++ eDB/apps/eDB/src/bootstrap.ts
@@ -1,4 +1,4 @@
-import { bootstrapWithKeycloak } from '@eDB/client-auth';
+import { bootstrapWithKeycloak } from '@edb/client-auth';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 bootstrapWithKeycloak(AppComponent, appConfig);
