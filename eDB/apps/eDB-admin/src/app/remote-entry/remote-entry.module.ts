141a87675041dcc58417632eee7137954a1f4e80
Elias <elias.db3@gmail.com>
2025-06-22 05:15:58 +0200

    test

---
+++ eDB/apps/eDB-admin/src/app/remote-entry/remote-entry.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { remoteRoutes } from './entry.routes';
+
+@NgModule({
+  imports: [RouterModule.forChild(remoteRoutes)],
+})
+export class RemoteAdminModule {}
