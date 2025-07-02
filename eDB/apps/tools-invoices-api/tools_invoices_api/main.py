6141592faaeae4493646b1957bb27782f7ece5cd
Elias <elias.db3@gmail.com>
2025-07-03 00:07:36 +0200

    added: first python api for invoice parsing

---
+++ eDB/apps/tools-invoices-api/tools_invoices_api/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .fetch_from_drive import fetch_all_invoice_data  # your utility function
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+async def startup_event():
+    global invoices_cache
+    invoices_cache = fetch_all_invoice_data()
+
+@app.get("/invoices")
+def get_invoices():
+    return invoices_cache
