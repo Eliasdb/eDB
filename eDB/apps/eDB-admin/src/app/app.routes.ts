bb9c4ac094336afceaa6586dfcdbf5c5dbe8fcd1
Elias <elias.db3@gmail.com>
2025-07-06 15:25:34 +0200

    test

--- eDB/apps/eDB-admin/src/app/app.routes.ts
+++ eDB/apps/eDB-admin/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Route } from '@angular/router';
 export const adminRemoteRoutes: Route[] = [
   {
     path: '',
-    pathMatch: 'full', // ★ add this
+    pathMatch: 'full',
 
     loadChildren: () =>
       import('@eDB/feature-admin-dashboard').then(
