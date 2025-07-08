2946b3cbed9dfd8acfddee5e8868f495b3f8d12f
Elias <elias.db3@gmail.com>
2025-07-08 13:56:59 +0200

    design

--- eDB/libs/shared-client/ui/src/lib/layouts/sidebar.layout.ts
+++ eDB/libs/shared-client/ui/src/lib/layouts/sidebar.layout.ts
@@ -48,14 +48,31 @@ export interface NavItem {
             (navClick)="leftNav.close()"
             (toggle)="leftNav.toggle()"
           >
-            <span sidebarLogo>Ω</span>
+            <span sidebarLogo>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-gallery-vertical-end size-4 mr-2 "
+                aria-hidden="true"
+              >
+                <path d="M7 2h10"></path>
+                <path d="M5 6h14"></path>
+                <rect width="18" height="12" x="3" y="10" rx="2"></rect></svg
+            ></span>
           </ui-nav-sidebar>
         </mat-drawer>
 
         <!-- ▣ MAIN CONTENT ---------------------------------------------- -->
         <mat-drawer-content class="bg-white pt-20">
           <header
-            class="h-16 flex items-center gap-2 px-6 border-b border-gray-200 text-sm text-black"
+            class="h-16 flex items-center gap-2 px-6 border-b border-solid border-[#e5e7eb]  text-sm text-black"
           >
             <svg
               (click)="leftNav.open()"
