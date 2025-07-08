8fe7d376773483d5ab5a0adb89b2025bdeb147ea
Elias <elias.db3@gmail.com>
2025-07-08 16:11:30 +0200

    added: loading skeleton catalog

--- eDB/libs/eDB/features/feature-catalog/src/lib/catalog.page.ts
+++ eDB/libs/eDB/features/feature-catalog/src/lib/catalog.page.ts
@@ -5,6 +5,7 @@ import {
   ListItem,
   NotificationModule,
   NotificationService,
+  SkeletonModule,
 } from 'carbon-components-angular';
 
 interface CatalogItem {
@@ -18,7 +19,12 @@ interface CatalogItem {
 @Component({
   selector: 'lib-catalog', // ✅ fixed prefix
   standalone: true,
-  imports: [UiTileComponent, UiComboboxComponent, NotificationModule],
+  imports: [
+    UiTileComponent,
+    UiComboboxComponent,
+    NotificationModule,
+    SkeletonModule,
+  ],
   providers: [NotificationService],
   template: `
     <section
@@ -34,25 +40,27 @@ interface CatalogItem {
       </section>
 
       <section class="relative z-10 w-full mt-8 mb-16 px-6">
-        @if (!isLoading()) {
-          @if (catalog() && catalog().length > 0) {
-            <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-              @for (item of catalog(); track item.name) {
-                <ui-tile
-                  [title]="item.name"
-                  [description]="item.description"
-                  [tags]="item.tags"
-                  [id]="item.id"
-                  [isSubscribed]="item.isSubscribed"
-                  (subscribe)="onSubscribe(item.id)"
-                ></ui-tile>
-              }
-            </div>
-          } @else {
-            <p class="text-white text-center">No catalog items found.</p>
-          }
+        @if (isLoading()) {
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            @for (_ of [1, 2, 3]; track _) {
+              <ui-tile [loading]="true"></ui-tile>
+            }
+          </div>
+        } @else if (catalog() && catalog().length > 0) {
+          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            @for (item of catalog(); track item.id) {
+              <ui-tile
+                [title]="item.name"
+                [description]="item.description"
+                [tags]="item.tags"
+                [id]="item.id"
+                [isSubscribed]="item.isSubscribed"
+                (subscribe)="onSubscribe(item.id)"
+              ></ui-tile>
+            }
+          </div>
         } @else {
-          <p class="text-gray-300 text-center">Loading catalog...</p>
+          <p class="text-white text-center">No catalog items found.</p>
         }
       </section>
     </section>
@@ -70,15 +78,13 @@ export class CatalogPageComponent {
   ];
 
   protected catalog = this.catalogService.catalog;
-  protected isLoading = this.catalogService.isLoading;
+  // protected isLoading = this.catalogService.isLoading;
+  protected isLoading = () => true; // Force loading state for testing
+
   protected error = this.catalogService.error;
 
   private toggleSubscribeMutation =
     this.catalogService.subscribeToApplicationMutation();
-
-  trackByName(index: number, item: CatalogItem): number {
-    return item.id;
-  }
 
   onSubscribe(appId: number) {
     const item = this.catalog().find((i) => i.id === appId);
--- eDB/libs/shared-client/ui/src/lib/components/tile/tile.component.ts
+++ eDB/libs/shared-client/ui/src/lib/components/tile/tile.component.ts
@@ -1,58 +1,89 @@
 import { Component, EventEmitter, input, model, Output } from '@angular/core';
-import { TilesModule } from 'carbon-components-angular';
+import { SkeletonModule, TilesModule } from 'carbon-components-angular';
 import { UiIconButtonComponent } from '../buttons/icon-button/icon-button.component';
 import { UiTagComponent } from '../tag/tag.component';
 
 @Component({
   selector: 'ui-tile',
-  imports: [TilesModule, UiTagComponent, UiIconButtonComponent],
-  template: `
-    <cds-tile
-      [class.subscribed]="isSubscribed()"
-      class="relative flex flex-col min-w-[20rem] bg-white  transition-colors duration-1000 ease-linear border border-width-2 rounded-[0.375rem]"
-    >
-      <div class="flex flex-col gap-4 ">
-        <h3 class="m-0 text-base text-[#1f2937] font-semibold">
-          {{ title() }}
-        </h3>
-        <p class="m-0 text-[0.9rem] text-[#6f6f6f] pr-8">{{ description() }}</p>
-      </div>
-      <div class="flex justify-between items-center">
-        <div class="flex flex-wrap">
-          @for (tag of tags(); track $index) {
-            <ui-tag [label]="tag"></ui-tag>
-          }
-        </div>
-
-        <div class="mb-3">
-          <ui-icon-button
-            buttonId="subscribe-btn"
-            size="sm"
-            icon="faDownload"
-            iconSize="16px"
-            iconColor="var(--accent)"
-            description="Subscribe"
-            (click)="emitSubscribe()"
-          ></ui-icon-button>
-        </div>
-      </div>
-    </cds-tile>
-  `,
+  standalone: true,
+  imports: [TilesModule, UiTagComponent, UiIconButtonComponent, SkeletonModule],
   styles: [
     `
       cds-tile.subscribed {
-        background-color: var(--surface-primary); /* Tailwind emerald‑100 */
-        border-color: var(--accent-complimentary); /* Tailwind emerald‑500 */
+        background-color: var(--surface-primary);
+        border-color: var(--accent-complimentary);
       }
     `,
   ],
+  template: `
+    <cds-tile
+      [class.subscribed]="isSubscribed()"
+      class="relative flex flex-col  bg-white transition-colors duration-300 ease-linear border rounded-[0.375rem] p-4"
+    >
+      @if (loading()) {
+        <div class="bg-white border rounded-[0.375rem] flex flex-col gap-4 ">
+          <cds-skeleton-placeholder
+            style="width: 20%; height: 1.25rem;"
+          ></cds-skeleton-placeholder>
+          <cds-skeleton-placeholder
+            style="width: 40%; height: 1.25rem;"
+          ></cds-skeleton-placeholder>
+
+          <div class="flex flex-wrap justify-between gap-2">
+            <section class="flex gap-2">
+              <cds-skeleton-placeholder
+                style="width: 3rem; height: 1rem;"
+              ></cds-skeleton-placeholder>
+              <cds-skeleton-placeholder
+                style="width: 3rem; height: 1rem;"
+              ></cds-skeleton-placeholder>
+            </section>
+
+            <cds-skeleton-placeholder
+              style="width: 2.25rem; height: 2.25rem; border-radius: 0.375rem; margin-botton:0.5rem;"
+            ></cds-skeleton-placeholder>
+          </div>
+        </div>
+      } @else {
+        <div class="flex flex-col gap-4">
+          <h3 class="m-0 text-base text-[#1f2937] font-semibold">
+            {{ title() }}
+          </h3>
+          <p class="m-0 text-[0.9rem] text-[#6f6f6f] pr-8">
+            {{ description() }}
+          </p>
+        </div>
+
+        <div class="flex justify-between items-center">
+          <div class="flex flex-wrap">
+            @for (tag of tags(); track $index) {
+              <ui-tag [label]="tag"></ui-tag>
+            }
+          </div>
+
+          <div class="mb-3">
+            <ui-icon-button
+              buttonId="subscribe-btn"
+              size="sm"
+              icon="faDownload"
+              iconSize="16px"
+              iconColor="var(--accent)"
+              description="Subscribe"
+              (click)="emitSubscribe()"
+            ></ui-icon-button>
+          </div>
+        </div>
+      }
+    </cds-tile>
+  `,
 })
 export class UiTileComponent {
   readonly id = input<number>();
-  readonly title = input.required<string>();
-  readonly description = input.required<string>();
+  readonly title = input<string>();
+  readonly description = input<string>();
   readonly tags = input<string[]>([]);
-  readonly isSubscribed = model<boolean>(); // new input for subscribed state
+  readonly isSubscribed = model<boolean>();
+  readonly loading = input(false); // new input
 
   @Output() subscribe = new EventEmitter<number>();
 
