f25d421177abe2b48ad6ee0331367bf26ca57364
Elias <elias.db3@gmail.com>
2025-07-11 16:03:16 +0200

    test?

--- eDB/docker/webshop-api/entrypoint.prod.sh
+++ eDB/docker/webshop-api/entrypoint.prod.sh
@@ -5,10 +5,10 @@ set -e
 ./wait-for-postgres.sh postgres-service-prod
 
 # Run migrations (only new ones, don’t drop data)
-php artisan migrate --force
+php artisan migrate
 
 # Run your custom book seeder (which truncates the books table itself)
-php artisan db:seed --class=CuratedBooksSeeder
+php artisan db:seed --class=CuratedBooksSeeder --force
 
 # Start PHP-FPM and Nginx
 php-fpm -D
