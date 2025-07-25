308de7e7d49185918029ede32fe6fe9efe8b9ad2
Elias <elias.db3@gmail.com>
2025-07-26 01:15:58 +0200

    test

--- eDB/apps/eDB-admin/src/app/remote-entry/entry.routes.ts
+++ eDB/apps/eDB-admin/src/app/remote-entry/entry.routes.ts
@@ -1,14 +1,15 @@
 // apps/eDB-admin/src/app/remote-entry/entry.routes.ts
-import { provideClientHydration } from '@angular/platform-browser';
+import { RendererFactory2 } from '@angular/core';
+import { ɵDomRendererFactory2 as DomRendererFactory2 } from '@angular/platform-browser';
 import { Route } from '@angular/router';
 
 export const adminRemoteRoutes: Route[] = [
   {
     path: '',
     providers: [
-      // importProvidersFrom(BrowserModule), // adds DomRendererFactory2
-      // provideAnimations(),
-      provideClientHydration(),
+      // provideClientHydration(),
+      // provideAnimations(), // ← optional, but now safe
+      { provide: DomRendererFactory2, useExisting: RendererFactory2 },
     ],
     loadChildren: () =>
       import('@eDB/feature-admin-dashboard').then(
