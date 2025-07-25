5958ef324a5f91828a674accf863c5cc6d45f55e
Elias <elias.db3@gmail.com>
2025-07-25 12:39:52 +0200

    fixed eslint config files

--- eDB/apps/eDB-user-account/eslint.config.cjs
+++ eDB/apps/eDB-user-account/eslint.config.cjs
@@ -1,12 +1,12 @@
-const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../eslint.config.js');
+import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.config.js';
 
-module.exports = [
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   ...baseConfig,
   ...nx.configs['flat/react'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
+    files: ['**/*.{ts,tsx,js,jsx}'],
     rules: {},
   },
 ];
