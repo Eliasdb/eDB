53ce5cd0681b412968e37dfa82f3fe91cdc65225
Elias <elias.db3@gmail.com>
2025-07-26 00:48:48 +0200

    test

--- eDB/apps/eDB-admin/src/app/remote-entry/entry.routes.ts
+++ eDB/apps/eDB-admin/src/app/remote-entry/entry.routes.ts
@@ -1,14 +1,13 @@
 // apps/eDB-admin/src/app/remote-entry/entry.routes.ts
-import { RendererFactory2 } from '@angular/core';
-import { ɵDomRendererFactory2 as DomRendererFactory2 } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Route } from '@angular/router';
 
 export const adminRemoteRoutes: Route[] = [
   {
     path: '',
     providers: [
-      { provide: DomRendererFactory2, useExisting: RendererFactory2 }, // single‑line bridge
-      /*  🚫  NO provideAnimations()  here */
+      importProvidersFrom(BrowserModule), // adds DomRendererFactory2
     ],
     loadChildren: () =>
       import('@eDB/feature-admin-dashboard').then(
