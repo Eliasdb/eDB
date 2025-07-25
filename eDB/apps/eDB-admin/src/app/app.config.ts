8912da3dafa7811139cef4c891663aaa9b86e1d9
Elias <elias.db3@gmail.com>
2025-07-25 22:31:25 +0200

    test

--- eDB/apps/eDB-admin/src/app/app.config.ts
+++ eDB/apps/eDB-admin/src/app/app.config.ts
@@ -1,31 +1,31 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { provideHttpClient, withFetch } from '@angular/common/http';
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
 
-import {
-  provideTanStackQuery,
-  QueryClient,
-} from '@tanstack/angular-query-experimental';
+// import {
+//   provideTanStackQuery,
+//   QueryClient,
+// } from '@tanstack/angular-query-experimental';
 
-import {
-  ExperimentalService,
-  ModalService,
-  NotificationDisplayService,
-  NotificationService,
-  PlaceholderService,
-} from 'carbon-components-angular';
-import { adminRemoteRoutes } from './app.routes';
+// import {
+//   ExperimentalService,
+//   ModalService,
+//   NotificationDisplayService,
+//   NotificationService,
+//   PlaceholderService,
+// } from 'carbon-components-angular';
+// import { adminRemoteRoutes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(adminRemoteRoutes),
-    provideTanStackQuery(new QueryClient()),
-    provideHttpClient(withFetch()),
-    NotificationService,
-    ModalService,
-    ExperimentalService,
-    NotificationDisplayService,
-    PlaceholderService,
-  ],
-};
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideZoneChangeDetection({ eventCoalescing: true }),
+//     provideRouter(adminRemoteRoutes),
+//     provideTanStackQuery(new QueryClient()),
+//     provideHttpClient(withFetch()),
+//     NotificationService,
+//     ModalService,
+//     ExperimentalService,
+//     NotificationDisplayService,
+//     PlaceholderService,
+//   ],
+// };
