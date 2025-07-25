0e5a170364536b4cede6651a372ddd7a46d22301
Elias <elias.db3@gmail.com>
2025-07-25 12:39:52 +0200

    fixed eslint config files

--- eDB/apps/eDB-admin/eslint.config.mjs
+++ eDB/apps/eDB-admin/eslint.config.mjs
@@ -1,7 +1,10 @@
-const nx = require('@nx/eslint-plugin');
-const baseConfig = require('../../eslint.config.js');
+// eslint.config.js or eslint.config.mjs
 
-module.exports = [
+import nx from '@nx/eslint-plugin';
+import baseConfig from '../../eslint.config.mjs';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   ...baseConfig,
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
@@ -28,7 +31,6 @@ module.exports = [
   },
   {
     files: ['**/*.html'],
-    // Override or add rules here
     rules: {},
   },
 ];
