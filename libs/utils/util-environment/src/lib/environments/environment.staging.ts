f92dda94825f21efc0ac91132ac2db120707f50e
Elias <elias.db3@gmail.com>
2025-08-13 20:52:49 +0200

    fix

--- libs/utils/util-environment/src/lib/environments/environment.staging.ts
+++ libs/utils/util-environment/src/lib/environments/environment.staging.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: false,
   apiBaseUrl: 'https://api.staging.eliasdebock.com/platform/api',
   apiAdminUrl: 'https://api.staging.eliasdebock.com/admin/api',
+  apiAdminStreamUrl: 'https://api.staging.eliasdebock.com/admin',
   bookAPIUrl: 'https://api.staging.eliasdebock.com/webshop/api/v1',
   invoicesAPIUrl: 'https://api.staging.eliasdebock.com',
 
